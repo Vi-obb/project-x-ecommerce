16e54c1a606a82b96c60e32430b3c3defc36b779
7heMagician <obbengvincent@gmail.com>
2022-09-24 03:02:42 +0000

    added data for products

---
+++ utils/data.js
@@ -0,0 +1,79 @@
+
+const data = {
+    products: [
+        {
+            name: 'Free Shirt',
+            slug: 'free-shirt',
+            category: 'Shirts',
+            image: '/images/shirt1.jpg',
+            price: 70,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 6,
+            description: 'high quality product'
+        },
+        {
+            name: 'Fit Shirt',
+            slug: 'fit-shirt',
+            category: 'Shirts',
+            image: '/images/shirt2.jpg',
+            price: 60,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 6,
+            description: 'fitting shirt'
+        },
+        {
+            name: 'Basic Shirt',
+            slug: 'basic-shirt',
+            category: 'Shirts',
+            image: '/images/shirt3.jpg',
+            price: 50,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 6,
+            description: 'basic shirt'
+        },
+        {
+            name: 'Best Pants',
+            slug: 'best-pants',
+            category: 'Pants',
+            image: '/images/pants1.jpg',
+            price: 120,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 6,
+            description: 'high quality product'
+        },
+        {
+            name: 'Golf Pants',
+            slug: 'golf-pants',
+            category: 'Pants',
+            image: '/images/pants2.jpg',
+            price: 100,
+            brand: 'Gucci',
+            rating: 3.5,
+            numReviews: 4,
+            countInStock: 34,
+            description: 'For the golfers'
+        },
+        {
+            name: 'Fit Pants',
+            slug: 'fit-pants',
+            category: 'Pants',
+            image: '/images/pants3.jpg',
+            price: 90,
+            brand: 'Off-White',
+            rating: 4.0,
+            numReviews: 12,
+            countInStock: 1,
+            description: 'Fitting pants'
+        }
+    ]
+}
+
+export default data
