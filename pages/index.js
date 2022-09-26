38d34bd5a47525bd4ac3455ffb7b2a6d5de9575c
7heMagician <obbengvincent@gmail.com>
2022-09-26 00:32:22 +0000

    created product deatils page

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
     <Layout title="Home">
       <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
         {data.products.map((product) => (
-          <ProductItem product={ product } key={ product.slug }></ProductItem>
+          <ProductItem product={product} key={product.slug}></ProductItem>
         ))}
       </div>
     </Layout>
